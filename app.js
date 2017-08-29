a4892b48b268603161c3e47e3d2a8b17902df6b2
lixiaochunoffice <490398311@qq.com>
2017-08-29 10:02:55 +0800

    done 使用connect-redis储存session

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const express = require('express')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session);
+const RedisStore = require('connect-redis')(session)
 const mongoose = require('mongoose')
 
 let app = express()
-// 数据库连接
-mongoose.connect('mongodb://127.0.0.1:27017/hubwiz'); //连接数据库
-mongoose.connection.on('open', function () {
-    console.log('-----------数据库连接成功！------------');
-});
+let options = {
+   "host": "127.0.0.1",
+   "port": "6379",
+   "ttl": 60 * 60 * 24 * 30,   //session的有效期为30天(秒)
+};
 
+// 此时req对象还没有session这个属性
 app.use(session({
-    secret: "what do you want to do?", //secret的值建议使用128个随机字符串
-    cookie: {maxAge: 60 * 1000 * 60 * 24 * 14}, //过期时间
-    resave: true, // 即使 session 没有被修改，也保存 session 值，默认为 true
-    saveUninitialized: true,
-    store: new MongoStore({
-        mongooseConnection: mongoose.connection //使用已有的数据库连接
-    })
+   store: new RedisStore(options),
+   secret: 'express is powerful'
 }));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   if (req.session.sign) {//检查用户是否已经登录
       console.log(req.session)//打印session的值
       res.send('welecome <strong>' + req.session.name + '</strong>, 欢迎你再次登录');
